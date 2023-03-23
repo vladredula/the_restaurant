b995820243662bcb5cb35e716d147dccce5afb02
Vladimer Redula <vdredula@gmail.com>
2023-03-23 17:15:42 +0900

    added route to menu controller

--- routes/web.php
+++ routes/web.php
@@ -16,3 +16,5 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/menu', [App\Http\Controllers\MenuController::class, 'index'])->name('menu');
