fea2b5fd3392232fd698f3956afcba1692346a4a
Vladimer Redula <vdredula@gmail.com>
2023-03-25 00:28:00 +0900

    added rest api for retrieving items from dynamodb

--- app/Http/Controllers/ItemController.php
+++ app/Http/Controllers/ItemController.php
@@ -2,18 +2,95 @@
 
 namespace App\Http\Controllers;
 
+use Illuminate\Support\Facades\Http;
 use Illuminate\Http\Request;
+use Illuminate\Support\Arr;
 
 class ItemController extends Controller
 {
     //
     public function food()
     {
-        return view('food');
+        $response = Http::get('https://3vflwnsyek.execute-api.ap-northeast-1.amazonaws.com/prod/items/food');
+    
+        $jsonData = $response->json();
+
+        $data = $this->unmarshal($jsonData['Items']);
+        $data = $this->reconstruct($data);
+
+        $category = $this->get_category('F');
+
+        return view('food', [
+            'items' => $data,
+            'categories' => $category
+        ]);
     }
 
     public function drink()
     {
-        return view('drink');
+        $response = Http::get('https://3vflwnsyek.execute-api.ap-northeast-1.amazonaws.com/prod/items/drink');
+    
+        $jsonData = $response->json();
+
+        $data = $this->reconstruct($jsonData['Items']);
+
+        return view('drink', [
+            'items' => $data['items'],
+            'category' => $data['category']
+        ]);
+    }
+
+    public function unmarshal($jsonData)
+    {
+        $new_data = [];
+
+        foreach ($jsonData as $data) {
+            $temp_data = array();
+
+            foreach ($data as $key => $value) {
+                $value_key = array_keys($value)[0];
+                $new_value = $value[$value_key];
+
+                if ($new_value === true) {
+                    $temp_data[$key] = "";
+                } 
+                else {
+                    $temp_data[$key] = $new_value;
+                }
+            }
+            
+            $new_data[] = $temp_data;
+        }
+
+        return $new_data;
+    }
+
+    public function reconstruct($data)
+    {
+        $new_data = array();
+
+        foreach ($data as $item) {
+            $category = '';
+            $sub_cat = 'none';
+
+            if ($item['subcategory'] != '') {
+                $sub_cat = $item['subcategory'];
+            }
+
+            $new_data[$item['category']][$sub_cat][] = $item;
+        }
+
+        return $new_data;
+    }
+
+    public function get_category($type = 'F')
+    {
+        $response = Http::get('https://3vflwnsyek.execute-api.ap-northeast-1.amazonaws.com/prod/category/'.$type);
+    
+        $jsonData = $response->json();
+
+        $data = $this->unmarshal($jsonData['Items']);
+
+        return $data;
     }
 }
