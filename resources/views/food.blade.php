251b0a59c7221a95d34052d89c07c279c247f249
Vladimer Redula <vdredula@gmail.com>
2023-03-25 00:28:32 +0900

    updated food view

--- resources/views/food.blade.php
+++ resources/views/food.blade.php
@@ -8,10 +8,9 @@
     <div class="row justify-content-center">
         <div class="col-md-11 col-lg-10 col-xl-9 col-xxl-8">
             <nav class="nav nav-pills flex-column flex-sm-row navbar-dark bg-dark">
-                <a class="flex-sm-fill text-center nav-link text-white rounded-0" data-bs-target="#menuCarousel" data-bs-slide-to="0" href="#">FOOD CAT1</a>
-                <a class="flex-sm-fill text-center nav-link text-white rounded-0" data-bs-target="#menuCarousel" data-bs-slide-to="1" aria-current=true href="#">FOOD CAT2</a>
-                <a class="flex-sm-fill text-center nav-link text-white rounded-0" data-bs-target="#menuCarousel" data-bs-slide-to="2" href="#">FOOD CAT3</a>
-                <a class="flex-sm-fill text-center nav-link text-white rounded-0" data-bs-target="#menuCarousel" data-bs-slide-to="3" href="#">FOOD CAT4</a>
+            @foreach ($categories as $category)
+                <a class="flex-sm-fill text-center nav-link text-white rounded-0" data-bs-toggle="collapse" data-bs-target="#foodCarousel" data-bs-slide-to="{{ $category['id'] }}" aria-current=true href="#{{ $category['abbreviation'] }}">{{ $category['name'] }}</a>
+            @endforeach
             </nav>
             <div id="menuCarousel" class="carousel slide carousel-fade" data-bs-ride="carousel">
                 <div class="carousel-inner">
@@ -31,129 +30,32 @@
             </div>
         </div>
     </div>
-    <div class="row justify-content-center">
+    @foreach ($categories as $category)
+    <div class="row collapse justify-content-center" id="{{ $category['abbreviation'] }}">
         <div class="col-md-11 col-lg-10 col-xl-9 col-xxl-8">
+        @foreach ($items[$category['abbreviation']] as $subcategory => $item)
+            @if ($subcategory != 'none')
             <div class="d-grid pt-3">
-                <button href="#foodcatlist1" data-bs-toggle="collapse" class="btn btn-danger rounded-0 text-start">Food sub cat 1</button>
+                <button href="#{{ $subcategory }}" data-bs-toggle="collapse" class="btn btn-danger rounded-0 text-start">{{ $subcategory }}</button>
             </div>
-            <div class="row collapse show" id="foodcatlist1">
+            @endif
+            <div class="row collapse" id="{{ $subcategory }}">
+                @foreach ($item as $food)
                 <div class="col-sm-6 col-md-4 col-lg-3 py-3">
                     <div class="card rounded-0 bg-dark">
-                        <img src="https://www.tgifridays.co.jp/wp/wp-content/uploads/2015/06/Burger-Fridays-Cheeseburger.jpg" class="card-img-top rounded-0" alt="...">
+                        <img src="{{ $food['img_url'] }}" class="card-img-top rounded-0" alt="...">
                         <div class="card-body">
-                            <h5 class="card-title text-white">Food 1</h5>
-                            <h6 class="card-subtitle mb-2 text-muted">Translation</h6>
-                            <p class="card-text text-white">Food Description blah blah blah blah blah blah blah blah blah.</p>
-                            <p class="card-text text-white">9,999円(Tax included 9,999円)</p>
+                            <h5 class="card-title text-white">{{ $food['name'] }}</h5>
+                            <h6 class="card-subtitle mb-2 text-muted">{{ $food['tname'] }}</h6>
+                            <p class="card-text text-white">{{ '¥'.$food['price'] }}</p>
                         </div>
                     </div>
                 </div>
-                <div class="col-sm-6 col-md-4 col-lg-3 py-3">
-                    <div class="card rounded-0 bg-dark">
-                        <img src="https://www.tgifridays.co.jp/wp/wp-content/uploads/2015/06/Burger-Fridays-Cheeseburger.jpg" class="card-img-top rounded-0" alt="...">
-                        <div class="card-body">
-                            <h5 class="card-title text-white">Food 1</h5>
-                            <h6 class="card-subtitle mb-2 text-muted">Translation</h6>
-                            <p class="card-text text-white">Food Description blah blah blah blah blah blah blah blah blah.</p>
-                            <p class="card-text text-white">9,999円(Tax included 9,999円)</p>
-                        </div>
-                    </div>
-                </div>
-                <div class="col-sm-6 col-md-4 col-lg-3 py-3">
-                    <div class="card rounded-0 bg-dark">
-                        <img src="https://www.tgifridays.co.jp/wp/wp-content/uploads/2015/06/Burger-Fridays-Cheeseburger.jpg" class="card-img-top rounded-0" alt="...">
-                        <div class="card-body">
-                            <h5 class="card-title text-white">Food 1</h5>
-                            <h6 class="card-subtitle mb-2 text-muted">Translation</h6>
-                            <p class="card-text text-white">Food Description blah blah blah blah blah blah blah blah blah.</p>
-                            <p class="card-text text-white">9,999円(Tax included 9,999円)</p>
-                        </div>
-                    </div>
-                </div>
-                <div class="col-sm-6 col-md-4 col-lg-3 py-3">
-                    <div class="card rounded-0 bg-dark">
-                        <img src="https://www.tgifridays.co.jp/wp/wp-content/uploads/2015/06/Burger-Fridays-Cheeseburger.jpg" class="card-img-top rounded-0" alt="...">
-                        <div class="card-body">
-                            <h5 class="card-title text-white">Food 1</h5>
-                            <h6 class="card-subtitle mb-2 text-muted">Translation</h6>
-                            <p class="card-text text-white">Food Description blah blah blah blah blah blah blah blah blah.</p>
-                            <p class="card-text text-white">9,999円(Tax included 9,999円)</p>
-                        </div>
-                    </div>
-                </div>
-                <div class="col-sm-6 col-md-4 col-lg-3 py-3">
-                    <div class="card rounded-0 bg-dark">
-                        <img src="https://www.tgifridays.co.jp/wp/wp-content/uploads/2015/06/Burger-Fridays-Cheeseburger.jpg" class="card-img-top rounded-0" alt="...">
-                        <div class="card-body">
-                            <h5 class="card-title text-white">Food 1</h5>
-                            <h6 class="card-subtitle mb-2 text-muted">Translation</h6>
-                            <p class="card-text text-white">Food Description blah blah blah blah blah blah blah blah blah.</p>
-                            <p class="card-text text-white">9,999円(Tax included 9,999円)</p>
-                        </div>
-                    </div>
-                </div>
-                <div class="col-sm-6 col-md-4 col-lg-3 py-3">
-                    <div class="card rounded-0 bg-dark">
-                        <img src="https://www.tgifridays.co.jp/wp/wp-content/uploads/2015/06/Burger-Fridays-Cheeseburger.jpg" class="card-img-top rounded-0" alt="...">
-                        <div class="card-body">
-                            <h5 class="card-title text-white">Food 1</h5>
-                            <h6 class="card-subtitle mb-2 text-muted">Translation</h6>
-                            <p class="card-text text-white">Food Description blah blah blah blah blah blah blah blah blah.</p>
-                            <p class="card-text text-white">9,999円(Tax included 9,999円)</p>
-                        </div>
-                    </div>
-                </div>
+                @endforeach
             </div>
-            <div class="d-grid pt-2">
-                <button href="#foodcatlist2" data-bs-toggle="collapse" class="btn btn-danger rounded-0 text-start">Food sub cat 1</button>
-            </div>
-            <div class="row py-3 collapse" id="foodcatlist2">
-                <div class="col col-sm-6 col-md-4 col-xl-3">
-                    <div class="card">
-                        <img src="https://www.tgifridays.co.jp/wp/wp-content/uploads/2015/06/Burger-Fridays-Cheeseburger.jpg" class="card-img-top" alt="...">
-                        <div class="card-body">
-                            <h5 class="card-title">Food 1</h5>
-                            <h6 class="card-subtitle mb-2 text-muted">Translation</h6>
-                            <p class="card-text">Food Description blah blah blah blah blah blah blah blah blah.</p>
-                            <p class="card-text">9,999円(Tax included 9,999円)</p>
-                        </div>
-                    </div>
-                </div>
-                <div class="col-sm-6 col-md-4 col-xl-3">
-                    <div class="card">
-                        <img src="https://www.tgifridays.co.jp/wp/wp-content/uploads/2015/06/Burger-Fridays-Cheeseburger.jpg" class="card-img-top" alt="...">
-                        <div class="card-body">
-                            <h5 class="card-title">Food 1</h5>
-                            <h6 class="card-subtitle mb-2 text-muted">Translation</h6>
-                            <p class="card-text">Food Description blah blah blah blah blah blah blah blah blah.</p>
-                            <p class="card-text">9,999円(Tax included 9,999円)</p>
-                        </div>
-                    </div>
-                </div>
-                <div class="col-sm-6 col-md-4 col-xl-3">
-                    <div class="card">
-                        <img src="https://www.tgifridays.co.jp/wp/wp-content/uploads/2015/06/Burger-Fridays-Cheeseburger.jpg" class="card-img-top" alt="...">
-                        <div class="card-body">
-                            <h5 class="card-title">Food 1</h5>
-                            <h6 class="card-subtitle mb-2 text-muted">Translation</h6>
-                            <p class="card-text">Food Description blah blah blah blah blah blah blah blah blah.</p>
-                            <p class="card-text">9,999円(Tax included 9,999円)</p>
-                        </div>
-                    </div>
-                </div>
-                <div class="col-sm-6 col-md-4 col-xl-3">
-                    <div class="card">
-                        <img src="https://www.tgifridays.co.jp/wp/wp-content/uploads/2015/06/Burger-Fridays-Cheeseburger.jpg" class="card-img-top" alt="...">
-                        <div class="card-body">
-                            <h5 class="card-title">Food 1</h5>
-                            <h6 class="card-subtitle mb-2 text-muted">Translation</h6>
-                            <p class="card-text">Food Description blah blah blah blah blah blah blah blah blah.</p>
-                            <p class="card-text">9,999円(Tax included 9,999円)</p>
-                        </div>
-                    </div>
-                </div>
-            </div>
+        @endforeach
         </div>
     </div>
+    @endforeach
 </div>
 @endsection
