2ada164817673e2c6543af9805b2efe4e4eb6fec
Vladimer Redula <vdredula@gmail.com>
2023-03-30 19:48:50 +0900

    fix for drink view

--- resources/views/drink.blade.php
+++ resources/views/drink.blade.php
@@ -4,10 +4,10 @@
 <div class="container">
     <div class="row justify-content-center">
         <div class="col-xxl-11">
-            <ul class="nav nav-pills nav-fill bg-dark">
+            <ul class="nav nav-pills nav-fill bg-light">
             @foreach ($categories as $id => $category)
-                <li class="nav-item mx-auto {{ (isset($active) && $active == $category['abbreviation']) ? 'active' : (!isset($active) && $id == 0 ? 'active' : '') }}">
-                    <a class="nav-link text-white rounded-0" href="{{ url('drink', ['category' => $category['abbreviation']]) }}">{{ strtoupper($category['name']) }}</a>
+                <li class="nav-item mx-auto">
+                    <a class="nav-link rounded-0 {{ (isset($active) && $active == $category['abbreviation']) ? 'active' : (!isset($active) && $id == 0 ? 'active' : '') }}" href="{{ url('drink', ['category' => $category['abbreviation']]) }}">{{ strtoupper($category['name']) }}</a>
                 </li>
             @endforeach
             </ul>
@@ -23,18 +23,52 @@
     <div class="row justify-content-center">
         <div class="col-xxl-11">
             <div class="row row-cols-sm-1 row-cols-md-2 row-cols-lg-3 row-col-xl-3">
+            @php
+                $marker = 0;
+                $useFlex = count($items) > 4 ? true : false;
+            @endphp
+            @if ($useFlex)
+                @foreach ($items as $subcat => $item)
+                    @if ($marker % 2 == 0)
+                    <div class="col">
+                        <div class="col d-flex flex-wrap">
+                    @endif
+                        <div class="col-12">
+                            <div class="d-grid pt-3">
+                                <button data-bs-target="#{{ preg_replace('/[^a-zA-Z]/', '', $subcat) }}" data-bs-toggle="collapse" class="btn btn-dark rounded-0 text-start">{{ ($subcat == 'none' ? $categories[0]['name'] : $subcat ) }}</button>
+                            </div>
+                            <ul class="list-group rounded-0 collapse show" id="{{ preg_replace('/[^a-zA-Z]/', '', $subcat) }}">
+                                @foreach ($item as $drink)
+                                <li class="list-group-item bg-light">{{ $drink['name'] }}</li>
+                                @endforeach
+                            </ul>
+                        </div>
+                    @if ($marker % 2 != 0)
+                        </div>
+                    </div>
+                    @endif
+                    @php
+                        $marker++;
+                    @endphp
+                @endforeach
+                @if ($marker % 2 == 0)
+                        </div>
+                    </div>
+                @endif
+            @else
             @foreach ($items as $subcategory => $item)
                 <div class="col" style="min-width: 260px;">
                     <div class="d-grid pt-3">
-                        <button data-bs-target="#{{ preg_replace('/[^a-zA-Z]/', '', $subcategory) }}" data-bs-toggle="collapse" class="btn btn-danger rounded-0 text-start">{{ ($subcategory == 'none' ? $categories[0]['name'] : $subcategory ) }}</button>
+                        <button data-bs-target="#{{ preg_replace('/[^a-zA-Z]/', '', $subcategory) }}" data-bs-toggle="collapse" class="btn btn-dark rounded-0 text-start">{{ ($subcategory == 'none' ? $categories[0]['name'] : $subcategory ) }}</button>
                     </div>
                     <ul class="list-group rounded-0 collapse show" id="{{ preg_replace('/[^a-zA-Z]/', '', $subcategory) }}">
                         @foreach ($item as $drink)
-                        <li class="list-group-item bg-dark text-white">{{ $drink['name'] }}</li>
+                        <li class="list-group-item bg-light">{{ $drink['name'] }}</li>
                         @endforeach
                     </ul>
                 </div>
             @endforeach
+            @endif
             </div>
         </div>
     </div>
