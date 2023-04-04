906f5a0a369af783313e1777874d6506a765f411
Vladimer Redula <vdredula@gmail.com>
2023-04-04 15:46:14 +0900

    added language.php in config and flags in scss

---
+++ config/languages.php
@@ -0,0 +1,12 @@
+<?php
+
+return [
+    'en' => [
+        'display' => 'English',
+        'flag-icon' => 'us'
+    ],
+    'ja' => [
+        'display' => '日本語',
+        'flag-icon' => 'jp'
+    ],
+];
