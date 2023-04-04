6c6007cb21f994a143429324579eb07de44aa378
Vladimer Redula <vdredula@gmail.com>
2023-04-04 15:48:02 +0900

    added language files

---
+++ lang/en/navbar.php
@@ -0,0 +1,22 @@
+<?php
+
+return [
+
+    /*
+    |--------------------------------------------------------------------------
+    | Navbar Language Lines
+    |--------------------------------------------------------------------------
+    |
+    | The following language lines are used during authentication for various
+    | messages that we need to display to the user. You are free to modify
+    | these language lines according to your application's requirements.
+    |
+    */
+
+    'login' => 'Login',
+    'register' => 'Register',
+    'logout' => 'Logout',
+    'food' => 'Foods',
+    'drink' => 'Drinks'
+
+];
