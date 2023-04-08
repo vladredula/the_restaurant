570143706b072857b0981b906376f180632ab9ea
Vladimer Redula <vdredula@gmail.com>
2023-04-08 18:03:15 +0900

    fixed for card layout

--- resources/views/food.blade.php
+++ resources/views/food.blade.php
@@ -4,7 +4,7 @@
 <div class="container">
     <div class="row justify-content-center">
         <div class="col-xxl-11">
-            <ul class="nav nav-pills nav-fill bg-light">
+            <ul class="nav nav-pills nav-fill nav-justified bg-light">
             @foreach ($categories as $id => $category)
                 <li class="nav-item mx-auto">
                     <button class="nav-link rounded-0 {{ $id == 0 ? 'active' : '' }}" data-bs-toggle="pill" data-bs-target="#foodCarousel" data-bs-slide-to="{{ $id }}" type="button" role="tab" aria-selected="{{ $id == 0 ? 'true' : 'false' }}">{{ strtoupper($category['name']) }}</button>
@@ -48,24 +48,24 @@
                                 <div class="row row-cols-sm-2 row-cols-md-3 row-cols-lg-4 row-cols-xl-5 d-flex flex-wrap justify-content-center">
                                 @foreach ($item as $food)
                                     <div class="col mx-auto px-3 pb-4">
-                                        <div class="card h-100 bg-light" style="min-width: 196px;">
-                                            <div class="row g-0">
+                                        <div class="card bg-light" style="min-width: 196px;">
+                                            <div class="row g-0 h-100">
                                                 <div class="col-5 col-sm-12">
                                                     <img src="{{ $food['img_url'] }}" class="card-img-top">
                                                 </div>
-                                                <div class="col-7 col-sm-12 h-100">
-                                                    <div class="card-body d-flex flex-column">
+                                                <div class="col-7 col-sm-12 d-flex flex-column">
+                                                    <div class="card-body flex-grow-1">
                                                         <p class="card-title fw-bold">
                                                             {{ App::getLocale() != 'en' ? $food['tname'] : $food['name'] }}
                                                         </p>
-                                                        <div class="text-muted text-end mt-auto">
+                                                    </div>
+                                                    <div class="card-footer text-muted text-end bg-light">
                                                         @foreach ($food['price'] as $size => $price)
                                                             {{ $size != '1' ? __('content.'.strtolower($size)) : "" }} {{ ($price) }}円  
                                                             @if (count($food['price']) > 1)
                                                                 <br>
                                                             @endif
                                                         @endforeach
-                                                        </div>
                                                     </div>
                                                 </div>
                                             </div>
