bc987b6a63ec569fbe57caeaef9e896f18dd3f7b
Vladimer Redula <vdredula@gmail.com>
2023-04-15 23:08:36 +0900

    extending dynamodb model

--- app/Models/User.php
+++ app/Models/User.php
@@ -2,15 +2,16 @@
 
 namespace App\Models;
 
-// use Illuminate\Contracts\Auth\MustVerifyEmail;
-use Illuminate\Database\Eloquent\Factories\HasFactory;
-use Illuminate\Foundation\Auth\User as Authenticatable;
-use Illuminate\Notifications\Notifiable;
-use Laravel\Sanctum\HasApiTokens;
+use Kitar\Dynamodb\Model\Model;
+use Illuminate\Auth\Authenticatable;
+use Illuminate\Contracts\Auth\Authenticatable as AuthenticatableContract;
 
-class User extends Authenticatable
+class User extends Model implements AuthenticatableContract
 {
-    use HasApiTokens, HasFactory, Notifiable;
+    use Authenticatable;
+
+    protected $table = 'users';
+    protected $primaryKey = 'email';
 
     /**
      * The attributes that are mass assignable.
