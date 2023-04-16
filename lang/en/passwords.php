d01883ed4a955a105812788e05f77f6086ca8688
Vladimer Redula <vdredula@gmail.com>
2023-04-16 11:28:28 +0900

    added new entry

--- lang/en/passwords.php
+++ lang/en/passwords.php
@@ -16,6 +16,7 @@ return [
     'reset' => 'Your password has been reset.',
     'sent' => 'We have emailed your password reset link.',
     'throttled' => 'Please wait before retrying.',
+    'check_email' => 'A password reset link was already been sent to this email. Please check your email.',
     'token' => 'This password reset token is invalid.',
     'user' => "We can't find a user with that email address.",
 
